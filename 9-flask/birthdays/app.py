91aa36c89cab0315bd94d801e9b534b9981573f9
Mohamed TCHOKETCH <mohamedtchoketch0@gmail.com>
2025-10-01 23:20:58 +0200

    Add birthdays

--- 9-flask/birthdays/app.py
+++ 9-flask/birthdays/app.py
@@ -27,13 +27,44 @@ def index():
     if request.method == "POST":
 
         # TODO: Add the user's entry into the database
+        # Access form data
+        # Access form data
+        name = request.form.get("name")
+        if not name:
+            return redirect("/")
+
+        month = request.form.get("month")
+        if not month:
+            return redirect("/")
+        try:
+            month = int(month)
+        except ValueError:
+            return redirect("/")
+        if month < 1 or month > 12:
+            return redirect("/")
+
+        day = request.form.get("day")
+        if not day:
+            return redirect("/")
+        try:
+            day = int(day)
+        except ValueError:
+            return redirect("/")
+        if day < 1 or day > 31:
+            return redirect("/")
+
+        # Insert data into database
+        db.execute(
+            "INSERT INTO birthdays (name, month, day) VALUES(?, ?, ?)", name, month, day
+        )
 
         return redirect("/")
 
     else:
 
         # TODO: Display the entries in the database on index.html
+        # Query for all birthdays
+        birthdays = db.execute("SELECT * FROM birthdays")
 
-        return render_template("index.html")
-
-
+        # Render birthdays page
+        return render_template("index.html", birthdays=birthdays)
